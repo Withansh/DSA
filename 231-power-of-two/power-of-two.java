296db86891edbf42503d4da716bed6f9107df9c3
Ansh Raj <anshsingh00680@gmail.com>
2026-05-22 23:59:17 +0530

    Time: 1 ms (96.70%) | Memory: 42.7 MB (18.17%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -1,16 +1,6 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        //base case 
-        if( n<=0){
-            return false;
-        }
-        //continue dividing as long as number is even
-        while (n %2 == 0){
-            n = n/2;
-        }
-
-        //if power of two we will be left exactly with 1
-        return n == 1;
+        return n > 0 && (n & (n - 1)) == 0;
+        
     }
-}        
-   
+}
